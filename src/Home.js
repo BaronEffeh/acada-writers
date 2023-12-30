4e048c3a4a0a8926d1865e2cc93fd0a1082dde36
Baron Effeh <effehbaron@gmail.com>
2023-12-30 22:32:58 +0100

    Select options and some CSS fixed

--- src/Home.js
+++ src/Home.js
@@ -6,45 +6,79 @@ const Home = () => {
             <section className="welcome">
                 <div className="welcome-content">
                     <h3>Create Top-notch Trusted Academic Statement of Purpose with us.</h3>
-                    <p>Acada writers helps you collect your professional experiences into an appeating document improving your chance of securing admission into your dream institution.</p>
-                    <button id="get-startrd">Get Started</button>
+                    <p>Acada writers help you collect your professional experiences into an appeating document improving your chance of securing admission into your dream institution.</p>
+                    <button id="get-started">Get Started</button>
                 </div>
                 <div className="table">
                     <table>
-                        <tr><td>Task Category <span id="down-arrow"><img src={process.env.PUBLIC_URL + '/Vector.png'} alt="Arrow down" /></span></td></tr>
-                        <tr><td>How soon do you need it? <span id="down-arrow"><img src={process.env.PUBLIC_URL + '/Vector.png'} alt="Arrow down" /></span></td></tr>
-                        <tr><td>Price<span id="price"><span id="naira">N</span>2,854</span></td></tr>
+                        <tbody>
+                            <td>
+                                <div className="custom-select">
+                                    <select name="tag-category" id="tag-category">
+                                        <option value="" disabled selected hidden>Task Category</option>
+                                        <option value="visas">Letter of Explanation - Visas</option>
+                                        <option value="application-essay">Statement of purpose - Admission/Application Essay</option>
+                                        <option value="letter-of-interest">Letter of Expression of Interest to potential professors</option>
+                                        <option value="uk-resume">UK styled resume</option>
+                                        <option value="canadian-resume">Canadian styled resume</option>
+                                    </select>
+                                    <span className="select-arrow"><img src={process.env.PUBLIC_URL + '/Vector.png'} alt="Arrow down" /></span>
+                                </div>
+                            </td>
+
+                            <tr>
+                                <td>
+                                    <div className="custom-select">
+                                        <select name="timeline" id="timeline">
+                                            <option value="" disabled selected hidden>How soon do you need it?</option>
+                                            <option value="15-days">15 days</option>
+                                            <option value="10-days">10 days</option>
+                                            <option value="7-days">7 days</option>
+                                            <option value="3-days">3 days</option>
+                                            <option value="1-day">1 day</option>
+                                        </select>
+                                        <span className="select-arrow"><img src={process.env.PUBLIC_URL + '/Vector.png'} alt="Arrow down" /></span>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Price<span id="price"><span id="naira">N</span>2,854</span>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </section>
         </section>
 
             <section className="about">
-                <div className="why-acada-writers">
-                    <hr />
-                    <h4>Why Acada Writers</h4>
-                    <p>Acada Writers brings you one-step closer to your goal. It is the one platform that aids in the simplification of your academic admission process.</p>
-                    <img src={process.env.PUBLIC_URL + '/about.png'} alt="About" />
-                </div>
-                <div className="value-privacy">
-                    <img src={process.env.PUBLIC_URL + '/value-added-services.png'} alt="Value added services" />
-                    <h5 id="value-added-services">Value added services</h5>
-                    <p>We make you focus on other aspects of your application process while we get you appealing personal statement.</p>
-                    <img src={process.env.PUBLIC_URL + '/privacy.png'} alt="Privacy" />
-                    <h5 id="privacy">Privacy</h5>
-                    <p>We don't share client's information with any third party as noted in our terms and condition. We provide confidentiality services.</p>
-                </div>
-                <div className="support-use">
-                    <img src={process.env.PUBLIC_URL + '/great-support.png'} alt="Great support" />
-                    <h5 id="great-support">Great Support</h5>
-                    <p>We are available 24/7. We have lots of staffs working around the clock to ensure we provide satisfaction to our client.</p>
-                    <img src={process.env.PUBLIC_URL + '/ease-of-use.png'} alt="Ease of use" />
-                    <h5 id="ease-of-use">Ease of use</h5>
-                    <p>Simple layout, easy to find information. Our platform is user-friendly, making it a breeze for you to understand and use.</p>
+                <div className="about-container">
+                    <div className="why-acada-writers">
+                        <hr />
+                        <h4>Why Acada Writers</h4>
+                        <p>Acada Writers brings you one-step closer to your goal. It is the one platform that aids in the simplification of your academic admission process.</p>
+                        <img src={process.env.PUBLIC_URL + '/about.png'} alt="About" />
+                    </div>
+                    <div className="value-privacy">
+                        <img src={process.env.PUBLIC_URL + '/value-added-services.png'} alt="Value added services" />
+                        <h5 id="value-added-services">Value added services</h5>
+                        <p>We make you focus on other aspects of your application process while we get you appealing personal statement.</p>
+                        <img src={process.env.PUBLIC_URL + '/privacy.png'} alt="Privacy" />
+                        <h5 id="privacy">Privacy</h5>
+                        <p>We don't share client's information with any third party as noted in our terms and condition. We provide confidentiality services.</p>
+                    </div>
+                    <div className="support-use">
+                        <img src={process.env.PUBLIC_URL + '/great-support.png'} alt="Great support" />
+                        <h5 id="great-support">Great Support</h5>
+                        <p>We are available 24/7. We have lots of staffs working around the clock to ensure we provide satisfaction to our client.</p>
+                        <img src={process.env.PUBLIC_URL + '/ease-of-use.png'} alt="Ease of use" />
+                        <h5 id="ease-of-use">Ease of use</h5>
+                        <p>Simple layout, easy to find information. Our platform is user-friendly, making it a breeze for you to understand and use.</p>
+                    </div>
                 </div>
             </section>
-            
-            <section className="contact">
+
+            <section id="contact">
                 <div className="contact-form">
                     <h4>Contact Us</h4>
                     <p>Reach out to our support team and get instant feedback.</p>
@@ -60,7 +94,7 @@ const Home = () => {
                         <button>Submit</button>
                     </form>
                 </div>
-            </section>        
+            </section>
         </>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,13 +6,13 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
+            <a href="#home"><img src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" /></a>
             <div className="menu">
-                <a href="/contact">Contact</a>
+                <a href="#contact">Contact</a>
             </div>
             <div className="links">
                 <a href="login">Login</a>
-                <button id="get-startrd">Get Started</button>
+                <button id="get-started">Get Started</button>
             </div>
         </nav>
     );
